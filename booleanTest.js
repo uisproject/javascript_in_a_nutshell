dbe65297dea3cef5d43e33fb1323cb23c8a7bfc9
uisproject <uisprojectxyz@gmail.com>
2022-05-31 11:33:18 +0700

    added booleantest

---
+++ booleanTest.js
@@ -0,0 +1,11 @@
+// so it comes to my mind that i want to know the meaning of false in JS
+// const x = null;          // false
+// const x = undefined;     // false
+// const x = [];            // true
+// const x = {}             // true
+// const x = [undefined];   // true
+// const x = [null];        // true
+// const x = Array();       // true
+
+if (x) console.log(true);
+else console.log(false);
