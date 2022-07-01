f1c1aca472abbdc5f4e3b9e82d64bfde195caa52
uisproject <uisprojectxyz@gmail.com>
2022-07-01 21:53:50 +0700

    added how to make debounce

---
+++ debounce.js
@@ -0,0 +1,20 @@
+// the logic of using debounce is to clear the timer and set it again
+// when the function is called again
+// this is useful for when you want to throttle a function
+// but you don't want to call it too often
+// for example, if you want to call sayHi() every second
+// but you don't want to call it more than once every second
+
+const debounce = (fn, delay) => {
+  let timer;
+  return (() => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(), delay);
+  })();
+};
+
+const sayHi = () => {
+  console.log("hi");
+};
+
+debounce(sayHi, 1000);
